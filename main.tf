6a9a7abbf3d0b67d65ea3d6b2444f1cd62c68c34
colereinke <48771293+colereinke@users.noreply.github.com>
2021-05-03 20:43:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ resource "aws_instance" "default" {
   key_name                    = var.ssh_key_pair
   subnet_id                   = var.subnet
   monitoring                  = var.detailed_monitoring
-  user_data                   = var.user_data
 
   vpc_security_group_ids = [(var.security_groups != "" ? var.security_groups : aws_security_group.default.id)]
 
